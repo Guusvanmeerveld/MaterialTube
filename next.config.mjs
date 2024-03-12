098ee2d8ba0bd42a0135003f896bb0d8316f0db2
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-03-12 23:06:35 +0100

    add next-pwa to turn website into pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import createPWA from "next-pwa";
+
+const withPWA = createPWA({
+	dest: "public"
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
