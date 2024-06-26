7447eb3fbfab9f64ece504c7b331351264262675
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-06-26 17:48:53 +0200

    channel: made avatar, banner and description optional

--- src/client/typings/channel.ts
+++ src/client/typings/channel.ts
@@ -3,7 +3,7 @@ import { Author } from "./author";
 export interface Channel extends Author {
 	id: string;
 	subscribers: number;
-	description: string;
-	avatar: string;
+	description?: string;
+	avatar?: string;
 	banner?: string;
 }
