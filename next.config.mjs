e4dd061a151c41d1a7a6c789e6c4a8021a261e75
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-03-19 21:10:35 +0100

    package: update deps + ducanh2912 -> serwist

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
-import withPWAInit from "@ducanh2912/next-pwa";
+import withSerwistInit from "@serwist/next";
 
-const withPWA = withPWAInit({
-	dest: "public"
+const withSerwist = withSerwistInit({
+	// Note: This is only an example. If you use Pages Router,
+	// use something else that works, such as "service-worker/index.ts".
+	swSrc: "src/sw.ts",
+	swDest: "public/sw.js"
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPWA(nextConfig);
+export default withSerwist(nextConfig);
